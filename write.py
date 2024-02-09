3af6f9ba219bb59ef185c823f0c0e89fe9c4a123
arce <2326arce@gmail.com>
2024-02-09 14:51:45 -0700

    todos

--- write.py
+++ write.py
@@ -16,6 +16,7 @@ def write_to_csv(catalog, search, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # Save search parameters
+    # TODO: Remove empty params
     search_filename = str(filename)[:-4] + "_search.csv"
     print("Saving search parameters to ", search_filename)
     with open(search_filename, "w") as fout:
@@ -23,6 +24,7 @@ def write_to_csv(catalog, search, filename):
         writer.writeheader()
         writer.writerow(search)
     # Save catalog
+    # TODO: expand articles list
     print("Saving catalog to ", filename)
     catalog.to_csv(filename)
 
@@ -39,6 +41,7 @@ def write_to_json(catalog, search, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # Save search parameters
+    # TODO: Remove empty params
     search_filename = str(filename)[:-5] + "_search.json"
     print("Saving search parameters to ", search_filename)
 
@@ -46,5 +49,6 @@ def write_to_json(catalog, search, filename):
         json.dump(search, fout, indent=2)
 
     # Save catalog
+    # TODO: clean format
     print("Saving catalog to ", filename)
     catalog.to_json(filename)
