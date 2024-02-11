958f448589175fee57b193973bed57f2bc69245a
arce <2326arce@gmail.com>
2024-02-11 10:47:41 -0700

    clean comments

--- write.py
+++ write.py
@@ -6,13 +6,8 @@ import json
 
 
 def write_to_csv(catalog, search, filename):
-    """Write an iterable of `CloseApproach` objects to a CSV file.
-
-    The precise output specification is in `README.md`. Roughly, each output row
-    corresponds to the information in a single close approach from the `results`
-    stream and its associated near-Earth object.
-
-    :param results: An iterable of `CloseApproach` objects.
+    """
+    :param : 
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # Save search parameters
@@ -30,14 +25,8 @@ def write_to_csv(catalog, search, filename):
 
 
 def write_to_json(catalog, search, filename):
-    """Write an iterable of `CloseApproach` objects to a JSON file.
-
-    The precise output specification is in `README.md`. Roughly, the output is a
-    list containing dictionaries, each mapping `CloseApproach` attributes to
-    their values and the 'neo' key mapping to a dictionary of the associated
-    NEO's attributes.
-
-    :param results: An iterable of `CloseApproach` objects.
+    """
+    :param : 
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # Save search parameters
